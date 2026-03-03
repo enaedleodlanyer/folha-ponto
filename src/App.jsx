d8a33a7b449beb056750fa5bb5d3943538f5f076
Allan Christian <allantecchristian23@gmail.com>
2026-03-03 15:25:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Folha Ponto Online</h1>
+      <p>Sistema funcionando corretamente 🎉</p>
+    </div>
+  )
+}
